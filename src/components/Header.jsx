c088f28f6098198050e348e520e2a614cc884c9f
Colin Moore <91088581+colinwebdev@users.noreply.github.com>
2025-01-24 17:51:47 -0800

    Added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <header>
+            <h1>Undead Orion</h1>
+        </header>
+    )
 }
 
 export default Header
